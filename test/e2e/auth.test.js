423915aac37dd7adb3518b5728be12e97ee3acd5
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-26 16:39:24 -0700

    signin, signup and post routes function

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -2,7 +2,7 @@ const { assert } = require('chai');
 const request = require('./request');
 const { dropCollection } = require('./db');
 
-describe('Auth E2E Test', () => {
+describe.only('Auth E2E Test', () => {
 
     before(() => dropCollection('reviewers'));
 
@@ -10,15 +10,42 @@ describe('Auth E2E Test', () => {
 
     before(() => {
         return request
-            .post('api/auth/signup')
+            .post('/auth/signup')
             .send({
+                name: 'Alan Cumming',
+                company: 'acting a fool',
                 email: 'me@me.com',
-                password: 'fuckdis'
+                password: 'buttstuff'
             })
             .then(({ body }) => token = body.token);
     });
 
     it('signup', () => {
         assert.ok(token);
+    });
+
+    it('signin', () => {
+        return request
+            .post('/auth/signin')
+            .send({
+                name: 'Alan Cumming',
+                company: 'acting a fool',
+                email: 'me@me.com',
+                password: 'buttstuff'
+            })
+            .then(({ body }) => {
+                assert.ok(body.token);
+            });
+    });
+
+    it('404 if signup with existing email', () => {
+        return request
+            .post('/auth/signup')
+            .send({
+                name: 'The Blowfish',
+                company: 'was it music?',
+                email: 'me@me.com',
+                password: 'meOhmy'
+            });
     });
 });
--- test/e2e/reviewer.test.js
+++ test/e2e/reviewer.test.js
@@ -2,7 +2,7 @@ const { assert } = require('chai');
 const request = require('./request');
 const { dropCollection } = require('./db');
 
-describe.skip('Reviewer API', () => {
+describe('Reviewer API', () => {
 
     before(() => dropCollection('studios'));
     before(() => dropCollection('actors'));
