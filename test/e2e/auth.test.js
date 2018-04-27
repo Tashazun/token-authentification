fc7cb897862d41d6669fe71ca01cb6a25de93a03
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-26 19:21:54 -0700

    authorization routes and tests function

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -24,6 +24,15 @@ describe.only('Auth E2E Test', () => {
         assert.ok(token);
     });
 
+    it('verifies', () => {
+        return request
+            .get('/auth/verify')
+            .set('Authorization', token)
+            .then(({ body }) => {
+                assert.isOk(body.verified);
+            });
+    });
+
     it('signin', () => {
         return request
             .post('/auth/signin')
@@ -38,7 +47,7 @@ describe.only('Auth E2E Test', () => {
             });
     });
 
-    it('404 if signup with existing email', () => {
+    it('400 if signup with existing email', () => {
         return request
             .post('/auth/signup')
             .send({
@@ -46,6 +55,38 @@ describe.only('Auth E2E Test', () => {
                 company: 'was it music?',
                 email: 'me@me.com',
                 password: 'meOhmy'
+            })
+            .then(res => {
+                assert.equal(res.status, 400);
             });
     });
+
+    it('401 if incorrect email', () => {
+        return request
+            .post('/auth/signin')
+            .send({                
+                name: 'Alan Cumming',
+                company: 'acting a fool',
+                email: 'you@me.com',
+                password: 'buttstuff'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+            });
+    });
+
+    // it('401 if incorrect password', () => {
+    //     return request
+    //         .post('/auth/signin')
+    //         .send({                
+    //             name: 'Alan Cumming',
+    //             company: 'acting a fool',
+    //             email: 'me@me.com',
+    //             password: 'warriorUnicorn'
+    //         })
+    //         .then(res => {
+    //             assert.equal(res.status, 401);
+    //         });
+    // });
+
 });
